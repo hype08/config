fd9a22390b73fdd1ee637dd08bb5617dea3029c8
Henry Zhang <henry.zhang@later.com>
2023-12-17 18:13:26 -0800

    new: add rest console

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Set leader key to space
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- Vim REST Console
+keymap.set("n", "<leader>xr", ":call VrcQuery()<CR>") -- Run REST query
