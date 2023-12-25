14ca7ab9d6cb673a2a2a471d8b675a4583c185b5
Henry Zhang <henry.zhang@later.com>
2023-12-25 11:23:30 -0800

    new: treesitter

---
+++ lua/plugins/nvim_treesitter.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    config = function()
+      -- import nvim-treesitter plugin
+      local treesitter = require("nvim-treesitter.configs")
+
+      -- configure treesitter
+      treesitter.setup({ -- enable syntax highlighting
+        highlight = {
+          enable = true,
+        },
+        indent = { enable = true },
+        ensure_installed = {
+          "bash",
+          "dockerfile",
+          "gitignore",
+          "go",
+          "graphql",
+          "javascript",
+          "json",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "ruby",
+          "tsx",
+          "typescript",
+          "vim",
+          "yaml",
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
+      })
+    end,
+  },
+}
