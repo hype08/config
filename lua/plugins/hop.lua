914184a65d82d7485d1e2a4cab5c20d8f3909d3c
Henry Zhang <henry.zhang@later.com>
2023-12-25 11:23:46 -0800

    new: hop character map

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -2,6 +2,7 @@ return {
   "smoka7/hop.nvim",
   keys = {
     { "<leader>h", ":HopWord<CR>", desc = "Hop to word" },
+    { "<leader>r", ":HopChar1<CR>", desc = "Hop to character" },
   },
   version = "*",
   opts = {},
