b16ad04cb4a15b9f09c3b788670768e68f7ccec0
Henry Zhang <henry.zhang@later.com>
2024-12-06 01:19:21 -0800

    more fun

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -52,5 +52,12 @@ keymap.set("n", "<leader>sfw", "<cmd>vertical resize<CR>", { desc = "Reset curre
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
-keymap.set("n", "< leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew<CR>", { desc = "Open current buffer in new tab" })
+
+-- Lols
+keymap.set("n", "yc", "yy<cmd>normal gcc<CR>p", { desc = "Duplicate a line and comment out the first line" })
+keymap.set("n", "<C-c>", "ciw", { desc = "Change word" })
+keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected up" }) -- thanks Prime
+keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected down" })
+keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search" })
