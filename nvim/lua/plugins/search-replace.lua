aa3ef60fc653a0a245017322a3b83273d702c9b3
Henry Zhang <henry.zhang@later.com>
2024-12-07 08:17:55 -0800

    map more search

--- nvim/lua/plugins/search-replace.lua
+++ nvim/lua/plugins/search-replace.lua
@@ -3,12 +3,20 @@ return {
   config = function()
     require("grug-far").setup({})
     local keymap = vim.keymap
-    keymap.set("n", "<leader>sr", function()
-      require("grug-far").open({ prefills = { search = vim.fn.expand("<cword>") } })
-    end, { desc = "GrugFar Search with current word" })
 
+    -- Main search toggle/open
+    keymap.set("n", "<leader>sr", function()
+      require("grug-far").toggle_instance({ instanceName = "far", staticTitle = "Find and Replace" })
+    end, { desc = "GrugFar Search Toggle" })
+
+    -- Search in current file only
     keymap.set("n", "<leader>srf", function()
       require("grug-far").open({ prefills = { paths = vim.fn.expand("%") } })
     end, { desc = "GrugFar Search limited to current file" })
+
+    -- Search with word under cursor
+    keymap.set("n", "<leader>sw", function()
+      require("grug-far").open({ prefills = { search = vim.fn.expand("<cword>") } })
+    end, { desc = "GrugFar Search with current word" })
   end,
 }
