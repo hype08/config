72c619ca61cc87e7298cd08c14d2894904b84a1a
Henry Zhang <henry.zhang@later.com>
2024-12-11 00:04:27 -0800

    map more lspsaga

--- nvim/lua/plugins/lspsaga.lua
+++ nvim/lua/plugins/lspsaga.lua
@@ -5,6 +5,10 @@ return {
     local keymap = vim.keymap
 
     keymap.set("n", "<C-x>", "<Cmd>Lspsaga outline<CR>", { noremap = true, silent = true })
+    keymap.set("n", "<C-i>", "<Cmd>Lspsaga peek_definition<CR>", { noremap = true, silent = true })
+    keymap.set("n", "<C-t>", "<Cmd>Lspsaga peek_type_definition<CR>", { noremap = true, silent = true })
+    keymap.set("n", "<C-space>", "<Cmd>Lspsaga finder<CR>", { noremap = true, silent = true })
+    keymap.set("n", "<leader>;", "<Cmd>Lspsaga term_toggle<CR>", { noremap = true, silent = true })
   end,
   dependencies = {
     "nvim-treesitter/nvim-treesitter", -- optional
