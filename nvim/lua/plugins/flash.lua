2fdb4606dc9ffe85206dcc8409a0be8e67a445a2
Henry Zhang <henry.zhang@later.com>
2024-12-16 16:25:39 -0800

    max window

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -19,28 +19,37 @@ return {
   },
   keys = {
     {
-      "s", 
-      mode = { "n", "x", "o" }, 
-      function() require("flash").jump() end,
+      "s",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
       desc = "Flash jump",
     },
     {
       "S",
       mode = { "n", "o", "x" },
-      function() require("flash").treesitter() end,
+      function()
+        require("flash").treesitter()
+      end,
       desc = "Flash Treesitter",
     },
     {
       "r",
       mode = "o",
-      function() require("flash").remote() end,
+      function()
+        require("flash").remote()
+      end,
       desc = "Remote Flash",
     },
     {
       "R",
       mode = { "n", "o", "x" },
-      function() require("flash").treesitter_search() end,
+      function()
+        require("flash").treesitter_search()
+      end,
       desc = "Flash Treesitter Search",
     },
   },
 }
+
--- nvim/lua/plugins/windows.lua
+++ nvim/lua/plugins/windows.lua
@@ -5,9 +5,32 @@ return {
     "anuvyklack/animation.nvim",
   },
   config = function()
-    -- vim.o.winwidth = 10
-    -- vim.o.winminwidth = 10
-    -- vim.o.equalalways = false
-    require("windows").setup()
+    vim.o.winwidth = 5 -- Minimum width of active window
+    vim.o.winminwidth = 1 -- Minimum width of inactive windows
+    vim.o.equalalways = false -- Don't auto-resize windows
+
+    require("windows").setup({
+      autowidth = {
+        enable = true,
+        winwidth = 0, -- Let windows.nvim handle the sizing
+        filetype = {
+          help = 2,
+          qf = 1,
+        },
+      },
+      ignore = {
+        buftype = { "quickfix" },
+        filetype = { "NvimTree", "neo-tree", "undotree", "gundo" },
+      },
+      animation = {
+        enable = true,
+        duration = 300,
+      },
+    })
+
+    vim.keymap.set("n", "<C-w>z", function()
+      require("windows.commands").maximize()
+    end, { desc = "Maximize current window" })
   end,
 }
+
